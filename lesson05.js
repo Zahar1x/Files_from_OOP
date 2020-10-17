f39019ceccf1a53857d20f74ae613c8e25820c9d
Zaharix <72162539+Zahar1x@users.noreply.github.com>
2020-10-17 16:09:25 +0300

    Add files via upload

---
+++ lesson05.js
@@ -0,0 +1,13 @@
+L.CL(L.name);
+L.name = 'sdkgs';
+//L.__defineGetter__('name', function(){return 'YOU DIED';});
+L.CL(L.version);
+L.version = '2343.o412';
+//L.__defineGetter__('version', function(){return 'YOU DIED';});
+L.CL(L.date);
+L.date = '30.36.6155';
+//L.__defineGetter__('date', function(){return 'YOU DIED';});
+L.CL(L.author);
+L.author = 'Jkfsalfs;'
+//L.__defineGetter__('author', function(){return 'YOU DIED';});
+
